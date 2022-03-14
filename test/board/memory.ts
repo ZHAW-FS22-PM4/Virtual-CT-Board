084ca4be4eec413ea7cbc14b593a69bdf83dfc1a
Stefan <stefan-enz@hotmail.ch>
2022-03-14 20:41:52 +0100

    VCB-46: representation of memory

---
+++ test/board/memory.ts
@@ -0,0 +1,5 @@
+/*
+    Tests the memory representation of the board
+*/
+
+import { writeByte, writeBytes, writeHalfword, writeWord, readByte, readHalfword, readWord, clearMemory } from '../../src/board/memory'
