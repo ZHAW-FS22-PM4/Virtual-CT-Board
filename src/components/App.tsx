58b6ab6bd59b8026c8edddf9bdab3f92a769f5e1
Silvan <niggsil2@students.zhaw.ch>
2022-03-18 12:51:05 +0100

    Add low registers

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,26 @@
 import React from 'react'
+import {Register} from "./register/Register";
 
 export class App extends React.Component {
     render (): React.ReactNode {
         return (
-          <div>
-            <h1>Hello World</h1>
-          </div>
+            <div className="m-3">
+              <h1>Virtual CT Board</h1>
+              <div className="container">
+                <div className="row">
+                  <div className="col-sm">
+                    Code Editor ...
+                  </div>
+                  <div className="col-sm">
+                    <Register/>
+                  </div>
+                  <div className="col-sm">
+                    Board ...
+                  </div>
+                </div>
+              </div>
+
+            </div>
         )
     }
 }
---
+++ src/components/register/Register.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import './Register.css'
+
+export class Register extends React.Component {
+  render(): React.ReactNode {
+
+    // TODO: replace dummy register
+    const registers = [
+      {key: 'R0', value: '0x12345678'},
+      {key: 'R1', value: '0x36126354'},
+      {key: 'R2', value: '0xFD344623'},
+      {key: 'R3', value: '0x1545F112'},
+      {key: 'R4', value: '0x23465344'},
+      {key: 'R5', value: '0x123F4678'},
+      {key: 'R6', value: '0x1AC4CC78'},
+      {key: 'R7', value: '0x12345DE8'},
+    ]
+
+    return (
+        <div>
+          <h2>Register</h2>
+          <div className="register-container">
+            {registers.map(r =>
+                <div className="row mx-2 my-1">
+                  <div className="col-sm-4">
+                    {r.key}
+                  </div>
+                  <div className="col-sm-8">
+                    {r.value}
+                  </div>
+                </div>
+            )}
+          </div>
+        </div>
+    )
+  }
+
+
+}
