b9ec943478fd88c0386cd99c5c42d904e0e62313
krebsrap <krebsrap@students.zhaw.ch>
2022-03-23 21:12:42 +0100

    reformat App.tsx Required, so the build pipeline can run the build successfully

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,21 +3,17 @@ import React from 'react'
 import { CodeEditor } from './CodeEditor/CodeEditor'
 
 export class App extends React.Component {
-    render (): React.ReactNode {
-        return (
-          <div className="container-fluid">
-            <div className="row">
-              <div className="col-md-4">
-                <CodeEditor />
-              </div>
-              <div className="col-md-2">
-                Registers...
-              </div>
-              <div className="col-md-6">
-                CT Board...
-              </div>
-            </div>
+  render(): React.ReactNode {
+    return (
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-4">
+            <CodeEditor />
           </div>
-        )
-    }
+          <div className="col-md-2">Registers...</div>
+          <div className="col-md-6">CT Board...</div>
+        </div>
+      </div>
+    )
+  }
 }
