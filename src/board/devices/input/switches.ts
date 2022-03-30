6c834b1cfc4362c14fd636f4fbd54459bfd53a54
deflox <git@deflox.net>
2022-03-30 23:45:57 +0200

    add memory implementation for switches, tests are still missing though

--- src/board/devices/input/switches.ts
+++ src/board/devices/input/switches.ts
@@ -1,21 +1,76 @@
-import { Word } from 'types/binary'
+import { Byte, Word } from 'types/binary'
 import { Device } from 'board/devices/device'
 
+/**
+ * Represents switches I/O in memory.
+ *
+ * @author Leo Rudin
+ */
 export class Switches extends Device {
-  public startAddress = Word.fromUnsignedInteger(0x08000000)
-  public endAddress = Word.fromUnsignedInteger(0x081fffff)
+  private static readonly blockAddressList: Word[] = [
+    Word.fromUnsignedInteger(0x60000200),
+    Word.fromUnsignedInteger(0x60000201),
+    Word.fromUnsignedInteger(0x60000202),
+    Word.fromUnsignedInteger(0x60000203)
+  ]
+
+  private static readonly MAX_SWITCH_NUMBER: number = 31
+
+  public startAddress = Switches.blockAddressList[0]
+  public endAddress = Switches.blockAddressList[3]
   public isReadOnly = false
   public isVolatile = false
 
+  /**
+   * Returns true if switch with given position is switched on.
+   *
+   * @param position switch position to check (0-31)
+   * @returns true if switch is turned on
+   */
   public isOn(position: number): boolean {
-    throw new Error('Device not yet implemented.')
+    if (this.invalidPosition(position)) {
+      throw new Error(`Position ${position} does not exist.`)
+    }
+
+    return this.findSwitchByte(position).isBitSet(position % 8)
   }
 
+  /**
+   * Toggles the switch with the given position.
+   *
+   * @param position switch position to toggle (0-31)
+   */
   public toggle(position: number): void {
-    throw new Error('Device not yet implemented.')
+    if (this.invalidPosition(position)) {
+      throw new Error(`Position ${position} does not exist.`)
+    }
+
+    this.findSwitchByte(position).toggleBit(position % 8)
   }
 
+  /**
+   * Sets the switch at the given position to the given value.
+   *
+   * @param position switch position to set value to (0-31)
+   * @param isOn value to set the switch to
+   */
   public set(position: number, isOn: boolean): void {
-    throw new Error('Device not yet implemented.')
+    if (this.invalidPosition(position)) {
+      throw new Error(`Position ${position} does not exist.`)
+    }
+
+    if (isOn) {
+      this.findSwitchByte(position).setBit(position % 8)
+    } else {
+      this.findSwitchByte(position).clearBit(position % 8)
+    }
+  }
+
+  private invalidPosition(position: number): boolean {
+    return position < 0 || position > Switches.MAX_SWITCH_NUMBER
+  }
+
+  private findSwitchByte(position: number): Byte {
+    return this.readByte(Switches.blockAddressList[Math.floor(position / 8)])
   }
 }
--- test/types/binary/byte.test.ts
+++ test/types/binary/byte.test.ts
@@ -4,6 +4,18 @@ const byte_00 = Byte.fromUnsignedInteger(0)
 const byte_80 = Byte.fromUnsignedInteger(128)
 const byte_81 = Byte.fromUnsignedInteger(129)
 
+const byte_0101_1010 = Byte.fromUnsignedInteger(90)
+
+const byte_0000_0000 = Byte.fromUnsignedInteger(0)
+const byte_0000_0001 = Byte.fromUnsignedInteger(1)
+const byte_0000_0011 = Byte.fromUnsignedInteger(3)
+const byte_0000_0111 = Byte.fromUnsignedInteger(7)
+const byte_0000_1111 = Byte.fromUnsignedInteger(15)
+const byte_0001_1111 = Byte.fromUnsignedInteger(31)
+const byte_0011_1111 = Byte.fromUnsignedInteger(63)
+const byte_0111_1111 = Byte.fromUnsignedInteger(127)
+const byte_1111_1111 = Byte.fromUnsignedInteger(255)
+
 test('fromUnsignedInteger_validValues', () => {
   expect(Byte.fromUnsignedInteger(255).value).toBe(255)
   expect(Byte.fromUnsignedInteger(128).value).toBe(128)
@@ -58,3 +70,103 @@ test('toHexString', () => {
   expect(byte_80.toHexString()).toBe('80')
   expect(byte_81.toHexString()).toBe('81')
 })
+
+describe('test isBitSet function', () => {
+  test('should return if bit is set or not', () => {
+    expect(byte_1111_1111.isBitSet(0)).toBe(true)
+    expect(byte_1111_1111.isBitSet(1)).toBe(true)
+    expect(byte_1111_1111.isBitSet(2)).toBe(true)
+    expect(byte_1111_1111.isBitSet(3)).toBe(true)
+    expect(byte_1111_1111.isBitSet(4)).toBe(true)
+    expect(byte_1111_1111.isBitSet(5)).toBe(true)
+    expect(byte_1111_1111.isBitSet(6)).toBe(true)
+    expect(byte_1111_1111.isBitSet(7)).toBe(true)
+
+    expect(byte_0101_1010.isBitSet(0)).toBe(false)
+    expect(byte_0101_1010.isBitSet(1)).toBe(true)
+    expect(byte_0101_1010.isBitSet(2)).toBe(false)
+    expect(byte_0101_1010.isBitSet(3)).toBe(true)
+    expect(byte_0101_1010.isBitSet(4)).toBe(true)
+    expect(byte_0101_1010.isBitSet(5)).toBe(false)
+    expect(byte_0101_1010.isBitSet(6)).toBe(true)
+    expect(byte_0101_1010.isBitSet(7)).toBe(false)
+  })
+  test('should throw error if out of range', () => {
+    expect(() => byte_1111_1111.isBitSet(32)).toThrow(
+      new Error('Bit offset 32 not in range 0 >= to < 8')
+    )
+    expect(() => byte_1111_1111.isBitSet(66)).toThrow(
+      new Error('Bit offset 66 not in range 0 >= to < 8')
+    )
+    expect(() => byte_1111_1111.isBitSet(-1)).toThrow(
+      new Error('Bit offset -1 not in range 0 >= to < 8')
+    )
+  })
+})
+
+describe('test setBit function', () => {
+  test('set bit where already set', () => {
+    expect(byte_1111_1111.setBit(7)).toEqual(byte_1111_1111)
+    expect(byte_1111_1111.setBit(0)).toEqual(byte_1111_1111)
+    expect(byte_1111_1111.setBit(5)).toEqual(byte_1111_1111)
+    expect(byte_0101_1010.setBit(1)).toEqual(byte_0101_1010)
+    expect(byte_0101_1010.setBit(3)).toEqual(byte_0101_1010)
+    expect(byte_0101_1010.setBit(6)).toEqual(byte_0101_1010)
+  })
+
+  test('set bit where not set yet', () => {
+    expect(byte_0000_0000.setBit(0)).toEqual(byte_0000_0001)
+    expect(byte_0000_0001.setBit(1)).toEqual(byte_0000_0011)
+    expect(byte_0000_0011.setBit(2)).toEqual(byte_0000_0111)
+    expect(byte_0000_0111.setBit(3)).toEqual(byte_0000_1111)
+    expect(byte_0000_1111.setBit(4)).toEqual(byte_0001_1111)
+    expect(byte_0001_1111.setBit(5)).toEqual(byte_0011_1111)
+    expect(byte_0011_1111.setBit(6)).toEqual(byte_0111_1111)
+    expect(byte_0111_1111.setBit(7)).toEqual(byte_1111_1111)
+
+    let byte: Byte = byte_0101_1010.setBit(0).setBit(2).setBit(5).setBit(7)
+    expect(byte).toEqual(byte_1111_1111)
+  })
+})
+
+/*
+describe('test clearBit function', () => {
+  test('clear bit which was set', () => {
+    expect(word_ffffffff.clearBit(31)).toEqual(
+      Word.fromUnsignedInteger(0x7fffffff)
+    )
+    expect(word_ffffffff.clearBit(0)).toEqual(
+      Word.fromUnsignedInteger(0xfffffffe)
+    )
+    expect(word_ffffffff.clearBit(22)).toEqual(
+      Word.fromUnsignedInteger(0xffbfffff)
+    )
+    expect(word_00010000.clearBit(16)).toEqual(word_00000000)
+    expect(word_f0000001.clearBit(0)).toEqual(word_f0000000)
+    expect(word_f0000001.clearBit(30)).toEqual(
+      Word.fromUnsignedInteger(0xb0000001)
+    )
+  })
+  test('clear bit which is already 0', () => {
+    expect(word_00000000.clearBit(31)).toEqual(word_00000000)
+    expect(word_00000000.clearBit(0)).toEqual(word_00000000)
+    expect(word_00000000.clearBit(7)).toEqual(word_00000000)
+    expect(word_00010000.clearBit(15)).toEqual(word_00010000)
+    expect(word_00010000.clearBit(17)).toEqual(word_00010000)
+    expect(word_f0000001.clearBit(2)).toEqual(word_f0000001)
+    expect(word_f0000001.clearBit(13)).toEqual(word_f0000001)
+    expect(word_f0000001.clearBit(23)).toEqual(word_f0000001)
+  })
+})
+
+describe('test toggleBit function', () => {
+  test('toggle and toggle again', () => {
+    let result = word_ffffffff.toggleBit(21)
+    expect(result).toEqual(Word.fromUnsignedInteger(0xffdfffff))
+    expect(result.toggleBit(21)).toEqual(word_ffffffff)
+    result = word_00000000.toggleBit(10)
+    expect(result).toEqual(Word.fromUnsignedInteger(0x00000400))
+    expect(result.toggleBit(10)).toEqual(word_00000000)
+  })
+})
+*/
