d08215b9e2ddc05e926d1cb7d7a81435d9fa0fe5
krebsrap <krebsrap@students.zhaw.ch>
2022-03-31 08:42:40 +0200

    basic memory view frontend layout

--- src/components/memoryexplorer/index.tsx
+++ src/components/memoryexplorer/index.tsx
@@ -1,33 +1,54 @@
 import React from 'react'
-import { $enum } from 'ts-enum-util'
+
 
 import Board from 'board'
-import { Register } from 'board/registers'
+
 
 import './style.css'
+import { Word } from '../../types/binary'
 
-export class MemoryExplorerComponent extends React.Component<{}> {
+type memoryState = {
+  [key: string]: Word
+}
+
+export class MemoryExplorerComponent extends React.Component<{}, any> {
   constructor(props: {}) {
     super(props)
-    this.state = {
-      value: 'Please write an essay about your favorite DOM element.'
-    }
+    Board.processor.on('afterCycle', this.afterCycle)
+    this.state = this.getState()
   }
 
-  handleChange(event: { target: { value: any } }) {
-    this.setState({ value: event.target.value })
+  private getState() {
+    const state: memoryState = {}
+    state['0x20000000'] = Word.fromUnsignedInteger(0x20000000)
+    state['0x20000001'] = Word.fromUnsignedInteger(0x20000000)
+    state['0x20000002'] = Word.fromUnsignedInteger(0x20000000)
+    state['0x20000003'] = Word.fromUnsignedInteger(0x20000000)
+    state['0x20000004'] = Word.fromUnsignedInteger(0x20000000)
+    state['0x20000005'] = Word.fromUnsignedInteger(0x20000000)
+    state['0x20000006'] = Word.fromUnsignedInteger(0x20000000)
+    state['0x20000007'] = Word.fromUnsignedInteger(0x20000000)
+    state['0x20000008'] = Word.fromUnsignedInteger(0x20000000)
+    state['0x20000009'] = Word.fromUnsignedInteger(0x20000000)
+    state['0x20000009'] = Word.fromUnsignedInteger(0x20000000)
+    state['0x20000009'] = Word.fromUnsignedInteger(0x20000000)
+    return state
   }
-  handleSubmit(event: { preventDefault: () => void }) {
-    alert('An essay was submitted: ' + this.state)
-    event.preventDefault()
+
+  private afterCycle() {
+    this.setState(this.getState())
   }
 
   public render(): React.ReactNode {
     return (
       <div className="memory-container">
         <h4 className="title">Memory</h4>
-        <div className="memory">
-          <textarea id="outlined-basic" />
+        <div className="memory overflow-auto">
+          {Object.keys(this.state).map((key) => (
+            <div>
+              {key} : {this.state[key].toHexString()}
+            </div>
+          ))}
         </div>
       </div>
     )
