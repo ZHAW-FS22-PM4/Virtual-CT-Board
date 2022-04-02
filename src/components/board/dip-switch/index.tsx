72eea4fe2232069c72c7e237b7de0a57cbcd8bd5
ni507 <61540155+ni507@users.noreply.github.com>
2022-04-02 08:06:02 +0200

    Vcb 27 dip switches (#12)
    * Add board and raw dip switches
    
    * add memory implementation for switches, tests are still missing though
    
    * add more tests for byte bit operations
    
    * finish writing tests and fix bug discovered while testing
    
    * bit better structure
    
    * make switches read only
    
    * Add switch component functionality
    
    * fix prettier
    
    * refactor and nest dip-switch component in board
    
    * fix endIndex
    
    Co-authored-by: deflox <git@deflox.net>

---
+++ src/components/board/dip-switch/index.tsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import Board from '../../../board'
+
+import './style.css'
+
+type DipSwitchState = {
+  [key: number]: boolean
+}
+
+type DipSwitchProps = {
+  startIndex: number
+  size: number
+}
+
+export class DipSwitch extends React.Component<DipSwitchProps, DipSwitchState> {
+  private readonly endIndex: number
+
+  constructor(props: DipSwitchProps) {
+    super(props)
+    this.endIndex = this.props.startIndex + this.props.size - 1
+    this.state = this.getState()
+  }
+
+  private getState() {
+    const state: DipSwitchState = {}
+
+    for (let i = this.props.startIndex; i <= this.endIndex; i++) {
+      state[i] = Board.switches.isOn(i)
+    }
+
+    return state
+  }
+
+  private handleSwitch = (key: number): void => {
+    this.setState((state) => ({
+      [key]: !state[key]
+    }))
+    Board.switches.toggle(key)
+  }
+
+  public render(): React.ReactNode {
+    return (
+      <div className="dip-switch-container">
+        {Object.keys(this.state)
+          .map(Number)
+          .sort((n1, n2) => n2 - n1)
+          .map((key) => (
+            <div key={'switch-' + key}>
+              <div className="label">{'S' + key}</div>
+              <button
+                className={`switch ${
+                  this.state[key] ? 'switch-on' : 'switch-off'
+                }`}
+                onClick={() => this.handleSwitch(key)}
+              />
+            </div>
+          ))}
+      </div>
+    )
+  }
+}
--- test/types/binary/byte.test.ts
+++ test/types/binary/byte.test.ts
@@ -4,6 +4,18 @@ const byte_00 = Byte.fromUnsignedInteger(0)
 const byte_80 = Byte.fromUnsignedInteger(128)
 const byte_81 = Byte.fromUnsignedInteger(129)
 
+const byte_0101_1010 = Byte.fromUnsignedInteger(90)
+
+const byte_0000_0000 = Byte.fromUnsignedInteger(0)
+const byte_0000_0001 = Byte.fromUnsignedInteger(1)
+const byte_0000_0011 = Byte.fromUnsignedInteger(3)
+const byte_0000_0111 = Byte.fromUnsignedInteger(7)
+const byte_0000_1111 = Byte.fromUnsignedInteger(15)
+const byte_0001_1111 = Byte.fromUnsignedInteger(31)
+const byte_0011_1111 = Byte.fromUnsignedInteger(63)
+const byte_0111_1111 = Byte.fromUnsignedInteger(127)
+const byte_1111_1111 = Byte.fromUnsignedInteger(255)
+
 test('fromUnsignedInteger_validValues', () => {
   expect(Byte.fromUnsignedInteger(255).value).toBe(255)
   expect(Byte.fromUnsignedInteger(128).value).toBe(128)
@@ -57,4 +69,109 @@ test('toHexString', () => {
   expect(byte_00.toHexString()).toBe('00')
   expect(byte_80.toHexString()).toBe('80')
   expect(byte_81.toHexString()).toBe('81')
+})
+
+describe('test isBitSet function', () => {
+  test('should return if bit is set or not', () => {
+    expect(byte_1111_1111.isBitSet(0)).toBe(true)
+    expect(byte_1111_1111.isBitSet(1)).toBe(true)
+    expect(byte_1111_1111.isBitSet(2)).toBe(true)
+    expect(byte_1111_1111.isBitSet(3)).toBe(true)
+    expect(byte_1111_1111.isBitSet(4)).toBe(true)
+    expect(byte_1111_1111.isBitSet(5)).toBe(true)
+    expect(byte_1111_1111.isBitSet(6)).toBe(true)
+    expect(byte_1111_1111.isBitSet(7)).toBe(true)
+
+    expect(byte_0101_1010.isBitSet(0)).toBe(false)
+    expect(byte_0101_1010.isBitSet(1)).toBe(true)
+    expect(byte_0101_1010.isBitSet(2)).toBe(false)
+    expect(byte_0101_1010.isBitSet(3)).toBe(true)
+    expect(byte_0101_1010.isBitSet(4)).toBe(true)
+    expect(byte_0101_1010.isBitSet(5)).toBe(false)
+    expect(byte_0101_1010.isBitSet(6)).toBe(true)
+    expect(byte_0101_1010.isBitSet(7)).toBe(false)
+  })
+  test('should throw error if out of range', () => {
+    expect(() => byte_1111_1111.isBitSet(32)).toThrow(
+      new Error('Bit offset 32 not in range 0 >= to < 8')
+    )
+    expect(() => byte_1111_1111.isBitSet(66)).toThrow(
+      new Error('Bit offset 66 not in range 0 >= to < 8')
+    )
+    expect(() => byte_1111_1111.isBitSet(-1)).toThrow(
+      new Error('Bit offset -1 not in range 0 >= to < 8')
+    )
+  })
+})
+
+describe('test setBit function', () => {
+  test('set bit where already set', () => {
+    expect(byte_1111_1111.setBit(7)).toEqual(byte_1111_1111)
+    expect(byte_1111_1111.setBit(0)).toEqual(byte_1111_1111)
+    expect(byte_1111_1111.setBit(5)).toEqual(byte_1111_1111)
+    expect(byte_0101_1010.setBit(1)).toEqual(byte_0101_1010)
+    expect(byte_0101_1010.setBit(3)).toEqual(byte_0101_1010)
+    expect(byte_0101_1010.setBit(6)).toEqual(byte_0101_1010)
+  })
+
+  test('set bit where not set yet', () => {
+    expect(byte_0000_0000.setBit(0)).toEqual(byte_0000_0001)
+    expect(byte_0000_0001.setBit(1)).toEqual(byte_0000_0011)
+    expect(byte_0000_0011.setBit(2)).toEqual(byte_0000_0111)
+    expect(byte_0000_0111.setBit(3)).toEqual(byte_0000_1111)
+    expect(byte_0000_1111.setBit(4)).toEqual(byte_0001_1111)
+    expect(byte_0001_1111.setBit(5)).toEqual(byte_0011_1111)
+    expect(byte_0011_1111.setBit(6)).toEqual(byte_0111_1111)
+    expect(byte_0111_1111.setBit(7)).toEqual(byte_1111_1111)
+
+    let byte: Byte = byte_0101_1010.setBit(0).setBit(2).setBit(5).setBit(7)
+    expect(byte).toEqual(byte_1111_1111)
+  })
+})
+
+describe('test clearBit function', () => {
+  test('clear bit where already cleared', () => {
+    expect(byte_0000_0000.clearBit(7)).toEqual(byte_0000_0000)
+    expect(byte_0000_0000.clearBit(5)).toEqual(byte_0000_0000)
+    expect(byte_0000_0000.clearBit(0)).toEqual(byte_0000_0000)
+    expect(byte_0101_1010.clearBit(7)).toEqual(byte_0101_1010)
+    expect(byte_0101_1010.clearBit(5)).toEqual(byte_0101_1010)
+    expect(byte_0101_1010.clearBit(2)).toEqual(byte_0101_1010)
+    expect(byte_0101_1010.clearBit(0)).toEqual(byte_0101_1010)
+  })
+
+  test('clear bit which was set', () => {
+    expect(byte_0000_0001.clearBit(0)).toEqual(byte_0000_0000)
+    expect(byte_0000_0011.clearBit(1)).toEqual(byte_0000_0001)
+    expect(byte_0000_0111.clearBit(2)).toEqual(byte_0000_0011)
+    expect(byte_0000_1111.clearBit(3)).toEqual(byte_0000_0111)
+    expect(byte_0001_1111.clearBit(4)).toEqual(byte_0000_1111)
+    expect(byte_0011_1111.clearBit(5)).toEqual(byte_0001_1111)
+    expect(byte_0111_1111.clearBit(6)).toEqual(byte_0011_1111)
+    expect(byte_1111_1111.clearBit(7)).toEqual(byte_0111_1111)
+  })
+})
+
+describe('test toggleBit function', () => {
+  test('toggle from 0 to 1', () => {
+    expect(byte_0000_0000.toggleBit(0)).toEqual(byte_0000_0001)
+    expect(byte_0000_0001.toggleBit(1)).toEqual(byte_0000_0011)
+    expect(byte_0000_0011.toggleBit(2)).toEqual(byte_0000_0111)
+    expect(byte_0000_0111.toggleBit(3)).toEqual(byte_0000_1111)
+    expect(byte_0000_1111.toggleBit(4)).toEqual(byte_0001_1111)
+    expect(byte_0001_1111.toggleBit(5)).toEqual(byte_0011_1111)
+    expect(byte_0011_1111.toggleBit(6)).toEqual(byte_0111_1111)
+    expect(byte_0111_1111.toggleBit(7)).toEqual(byte_1111_1111)
+  })
+
+  test('toggle from 1 to 0', () => {
+    expect(byte_1111_1111.toggleBit(7)).toEqual(byte_0111_1111)
+    expect(byte_0111_1111.toggleBit(6)).toEqual(byte_0011_1111)
+    expect(byte_0011_1111.toggleBit(5)).toEqual(byte_0001_1111)
+    expect(byte_0001_1111.toggleBit(4)).toEqual(byte_0000_1111)
+    expect(byte_0000_1111.toggleBit(3)).toEqual(byte_0000_0111)
+    expect(byte_0000_0111.toggleBit(2)).toEqual(byte_0000_0011)
+    expect(byte_0000_0011.toggleBit(1)).toEqual(byte_0000_0001)
+    expect(byte_0000_0001.toggleBit(0)).toEqual(byte_0000_0000)
+  })
 })
