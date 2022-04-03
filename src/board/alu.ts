f88a3df86d98bc0dd8095309a9ad4a03916387c6
deflox <git@deflox.net>
2022-04-03 23:48:14 +0200

    add tests for sub function and fix bugs discovered while testing

--- src/board/alu.ts
+++ src/board/alu.ts
@@ -9,7 +9,7 @@ export type AluResult = {
 }
 
 /**
- * Adds the two words together and returns the result as well as the flags that resulted with this add operation.
+ * Adds the two words together and returns the result with the correclty set flags.
  *
  * @param o1 operant one
  * @param o2 operant two
@@ -28,21 +28,24 @@ export function add(o1: Word, o2: Word): AluResult {
 }
 
 /**
- * Subtracts the two operands from each other and returns the result as well as the flags that resulted with this add operation.
+ * Subtracts the two words from each other and returns the result with the correctly set flags.
  *
  * @param o1 operant one
  * @param o2 operant two
  * @returns alu result which contains the result word and the flags
  */
 export function sub(o1: Word, o2: Word): AluResult {
-  const complement: number = (~o2.toUnsignedInteger() + 1) >>> 0
-  const result: Word = o1.add(Word.fromUnsignedInteger(complement))
+  const complement: Word = Word.fromUnsignedInteger(
+    (~o2.toUnsignedInteger() + 1) >>> 0
+  )
+  const result: Word = o1.add(complement)
   return {
     result: result,
     N: result.hasSign(),
     Z: result.toUnsignedInteger() === 0,
     C:
-      o1.toUnsignedInteger() + o2.toUnsignedInteger() > Word.MAX_UNSIGNED_VALUE,
-    V: o1.hasSign() == o2.hasSign() && o1.hasSign() != result.hasSign()
+      o1.toUnsignedInteger() + complement.toUnsignedInteger() >
+      Word.MAX_UNSIGNED_VALUE,
+    V: o1.hasSign() != o2.hasSign() && o1.hasSign() != result.hasSign()
   }
 }
