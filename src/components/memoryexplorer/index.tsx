dae7d5409c9f791373c55732ae5b0bb011f03290
krebsrap <krebsrap@students.zhaw.ch>
2022-04-05 23:02:02 +0200

    implement scrolling and hex-bin switch

--- src/components/memoryexplorer/index.tsx
+++ src/components/memoryexplorer/index.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
 
-
 import Board from 'board'
 
-
 import './style.css'
-import {Byte, Word} from '../../types/binary'
+import { Byte, Word } from '../../types/binary'
 
-type MemoryState = {
-  [key: number]: Word
+interface MemoryState {
+  memory: Map<number, Word>
+  format: FormatType
+  startAddress: number
+  endAddress: number
 }
 
 enum FormatType {
@@ -20,41 +21,61 @@ export class MemoryExplorerComponent extends React.Component<{}, any> {
   constructor(props: {}) {
     super(props)
     Board.processor.on('afterCycle', () => this.update())
-    this.state = this.getState()
   }
 
+  state: MemoryState = {
+    memory: new Map<number, Word>(),
+    format: FormatType.HEX,
+    startAddress: 0x08000000,
+    endAddress: 0x08000080
+  }
 
   private update() {
     this.setState(this.getState())
   }
 
   private getState() {
-    const state: MemoryState = {}
-    const startAddress = 0x08000000
-    const endAddress = 0x08000160
-    if (Board.processor.isRunning()) {
-      for (let i = startAddress; i <= endAddress; i = i + 4) {
-        state[i] = Board.memory.readWord(Word.fromUnsignedInteger(i))
-      }
+    const newMemory: Map<number, Word> = new Map<number, Word>()
+    for (
+      let i = this.state.startAddress;
+      i <= this.state.endAddress;
+      i = i + 4
+    ) {
+      newMemory.set(i, Board.memory.readWord(Word.fromUnsignedInteger(i)))
     }
-    return state
+    return { memory: newMemory }
   }
 
-  private scrollUp() {
-
+  scrollUp = (): void => {
+    this.state.startAddress -= 16
+    this.state.endAddress -= 16
+    this.update()
   }
 
-  private scrollDown() {
-    const newState: MemoryState = this.state
+  scrollDown = (): void => {
+    this.state.startAddress += 16
+    this.state.endAddress += 16
+    this.update()
+  }
 
+  hexView = (): void => {
+    this.state.format = FormatType.HEX
+    this.update()
+  }
+
+  binView = (): void => {
+    this.state.format = FormatType.BIN
+    this.update()
   }
 
   public render(): React.ReactNode {
-    const memoryList = this.formatMemory(FormatType.HEX);
+    const memoryList = this.formatMemoryNew()
     return (
       <div className="memory-container">
         <h4 className="title">Memory</h4>
-        <table className="table table-striped table-hover" style={{width:70}}>
+        <table
+          className="table table-striped table-hover"
+          style={{ width: 70 }}>
           <thead>
             <tr className="memory-topbar">
               <th>Address</th>
@@ -63,72 +84,77 @@ export class MemoryExplorerComponent extends React.Component<{}, any> {
           </thead>
           <tbody>
             {memoryList.map((line) => (
-                <tr className="address">
-                  <th scope="row" className="w-25">{line[0]}</th>
-                  <td className="w-25">{line[1]}</td>
-                  <td className="w-25">{line[2]}</td>
-                  <td className="w-25">{line[3]}</td>
-                  <td className="w-25">{line[4]}</td>
-                </tr>
+              <tr className="address">
+                <th scope="row" className="w-25">
+                  {line[0]}
+                </th>
+                <td className="w-25">{line[1]}</td>
+                <td className="w-25">{line[2]}</td>
+                <td className="w-25">{line[3]}</td>
+                <td className="w-25">{line[4]}</td>
+              </tr>
             ))}
           </tbody>
         </table>
         <button onClick={this.scrollUp}>Up</button>
         <button onClick={this.scrollDown}>Down</button>
+        <div
+          className="btn-group"
+          role="group"
+          aria-label="Basic radio toggle button group">
+          <input
+            type="radio"
+            className="btn-check"
+            name="btnradio"
+            id="btnradio1"
+            autoComplete="off"
+            checked
+            onClick={this.hexView}
+          />
+          <label className="btn btn-outline-primary" htmlFor="btnradio1">
+            Hex View
+          </label>
+          <input
+            type="radio"
+            className="btn-check"
+            name="btnradio"
+            id="btnradio2"
+            autoComplete="off"
+            onClick={this.binView}
+          />
+          <label className="btn btn-outline-primary" htmlFor="btnradio2">
+            Binary View
+          </label>
+        </div>
       </div>
     )
   }
 
-
-
-  private formatMemory(formatType: FormatType) {
-    const memoryList = []
+  private formatMemoryNew() {
+    const memoryList: string[][] = []
     let currentLine: string[] = []
-    let lineCounter = 4
-    for (let i in this.state) {
-      const element = this.state[i]
-      if (lineCounter++ == 4) {
-        lineCounter = 0
-        if (currentLine.length > 0) memoryList.push(currentLine)
-        currentLine = []
-        if (formatType == FormatType.HEX) {
-          currentLine.push(Word.fromUnsignedInteger(Number.parseInt(i)).toHexString())
-          currentLine.push(element.toHexString().toUpperCase())
-        }
-        else {
-          currentLine.push(Word.fromUnsignedInteger(Number.parseInt(i)).toBinaryString())
-          currentLine.push(element.toBinaryString().toUpperCase())
-        }
-      } else {
-        if (formatType == FormatType.HEX) {
-          currentLine.push(element.toHexString().toUpperCase())
-        }
-        else {
-          currentLine.push(element.toBinaryString().toUpperCase())
-        }
+    let currentAddress: string
+    let lineCounter = 0
+    this.getState().memory.forEach((memoryEntry: Word, address: number) => {
+      console.log(memoryEntry.toHexString())
+      if (lineCounter == 0) {
+        currentAddress = Word.fromUnsignedInteger(address)
+          .toHexString()
+          .toUpperCase()
       }
-    }
-    memoryList.push(currentLine)
-    return memoryList;
+      if (this.state.format == FormatType.HEX) {
+        currentLine.push(memoryEntry.toHexString().toUpperCase())
+      } else {
+        currentLine.push(memoryEntry.toBinaryString())
+      }
+      if (lineCounter == 3) {
+        memoryList.push([currentAddress, ...currentLine])
+        currentLine = []
+        lineCounter = 0
+      } else {
+        lineCounter++
+      }
+    })
+    return memoryList
   }
 }
-/*
-
-      <div className="memory-container">
-        <h4 className="title">Memory</h4>
-        <div className="memory-topbar">
-          <div>Address</div>
-          <div>Memory contents</div>
-        </div>
-        <div className="memory">
-          {memoryList.map((line) => (
-              <div>
-                <div className="address">{line[0]}</div>
-                <span className="memory-entry">{line[1]} {line[2]} {line[3]} {line[4]}</span>
-              </div>
-
-          ))}
-        </div>
-      </div>
-
- */
