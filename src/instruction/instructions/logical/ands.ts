28287d36d60157a3d3ec87aee9f670986cb1ebd8
Phil <philippe-schneider@gmx.net>
2022-04-07 13:13:05 +0200

    VCB-12, VCB-99 Implemented ANDS

---
+++ src/instruction/instructions/logical/ands.ts
@@ -0,0 +1,59 @@
+
+import {BaseInstruction} from "../baseInstruction";
+import {
+    checkOptionCount,
+    create,
+    createLowRegisterBits,
+    getBits,
+    isImmediate,
+    isOptionCountValid,
+    setBits
+} from "../../opcode";
+import {Halfword, Word} from "../../../types/binary";
+import {IMemory} from "../../../board/memory/interfaces";
+import {Registers} from "../../../board/registers";
+import {ILabelOffsets} from "../../interfaces";
+
+/**
+ * Represents a 'STORE' instruction - STR (immediate offset) - word
+ */
+export class AndsInstruction extends BaseInstruction {
+    public name: string = 'ANDS'
+    public pattern: string =        '0100000000XXXXXX'
+    private rdnPattern: string =    '0100000000000XXX'
+    private rmPattern: string =     '0100000000XXX000'
+    private expectedOptionCount: number = 3
+
+    public canEncodeInstruction(name: string, options: string[]): boolean {
+        return (
+            super.canEncodeInstruction(name, options) &&
+            isOptionCountValid(options, this.expectedOptionCount) &&
+            (options[0] == options[1]) &&
+            !isImmediate(options[0]) &&
+            !isImmediate(options[2])
+        )
+    }
+
+    public encodeInstruction(options: string[], labels: ILabelOffsets): Halfword {
+        checkOptionCount(options, 3)
+        let opcode: Halfword = create(this.pattern)
+        opcode = setBits(opcode, this.rmPattern, createLowRegisterBits(options[2]))
+        opcode = setBits(opcode, this.rmPattern, createLowRegisterBits(options[0]))
+        return opcode
+    }
+
+    public executeInstruction(
+        opcode: Halfword,
+        registers: Registers,
+        memory: IMemory
+    ): void {
+        let valueToWrite = Word.fromUnsignedInteger(
+            (registers.readRegister(getBits(opcode, this.rdnPattern).value).toUnsignedInteger() &
+                registers.readRegister(getBits(opcode, this.rmPattern).value).toUnsignedInteger())
+        )
+        registers.setNegativeFlag(valueToWrite)
+        registers.setZeroFlag(valueToWrite.value)
+        registers.writeRegister(getBits(opcode, this.rdnPattern).value, valueToWrite)
+    }
+}
+
