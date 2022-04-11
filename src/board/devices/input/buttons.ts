8383a1109dd1f1bf5faabec947de72d307b081ae
Silvan <niggsil2@students.zhaw.ch>
2022-04-11 18:35:20 +0200

    add buttons memory representation

---
+++ src/board/devices/input/buttons.ts
@@ -0,0 +1,68 @@
+/**
+ * Represents buttons I/O in memory.
+ *
+ * @author Silvan Nigg
+ */
+import { Device } from 'board/devices/device'
+import { Word } from 'types/binary'
+
+export class Buttons extends Device {
+  private static readonly MAX_BUTTON_NUMBER: number = 3
+  private static readonly BUTTONS_ADDRESS: Word =
+    Word.fromUnsignedInteger(0x60000210)
+
+  public startAddress = Buttons.BUTTONS_ADDRESS
+  public endAddress = Buttons.BUTTONS_ADDRESS
+  public isReadOnly = true
+  public isVolatile = false
+
+  /**
+   * Returns true if button with given position is pressed.
+   *
+   * @param position button position to check (0-3)
+   * @returns true if button is pressed
+   */
+  public isPressed(position: number): boolean {
+    if (Buttons.invalidPosition(position)) {
+      throw new Error(`Button position ${position} does not exist.`)
+    }
+
+    return this.memory.readByte(Buttons.BUTTONS_ADDRESS).isBitSet(position)
+  }
+
+  /**
+   * Presses the button at the given position.
+   *
+   * @param position button position to press (0-3)
+   */
+  public press(position: number): void {
+    if (Buttons.invalidPosition(position)) {
+      throw new Error(`Button position ${position} does not exist.`)
+    }
+
+    const updatedByte = this.memory
+      .readByte(Buttons.BUTTONS_ADDRESS)
+      .setBit(position)
+    this.memory.writeByte(Buttons.BUTTONS_ADDRESS, updatedByte)
+  }
+
+  /**
+   * Releases the button at the given position.
+   *
+   * @param position button position to press (0-3)
+   */
+  public release(position: number): void {
+    if (Buttons.invalidPosition(position)) {
+      throw new Error(`button position ${position} does not exist.`)
+    }
+
+    const updatedByte = this.memory
+      .readByte(Buttons.BUTTONS_ADDRESS)
+      .clearBit(position)
+    this.memory.writeByte(Buttons.BUTTONS_ADDRESS, updatedByte)
+  }
+
+  private static invalidPosition(position: number): boolean {
+    return position < 0 || position > Buttons.MAX_BUTTON_NUMBER
+  }
+}
---
+++ test/board/devices/input/buttons.test.ts
@@ -0,0 +1,93 @@
+import { Buttons } from 'board/devices/input/buttons'
+import { Byte, Word } from 'types/binary'
+
+const address: Word = Word.fromUnsignedInteger(0x60000210)
+const byte_0000_0000: Byte = Byte.fromUnsignedInteger(0)
+const byte_0000_0101: Byte = Byte.fromUnsignedInteger(5)
+const byte_0000_1111: Byte = Byte.fromUnsignedInteger(15)
+
+let buttons: Buttons
+
+beforeEach(() => {
+  buttons = new Buttons()
+  buttons.press(0)
+  buttons.press(2)
+})
+
+test('test that buttons are set to readonly', () => {
+  buttons.writeByte(address, byte_0000_0000)
+  expect(buttons.readByte(address)).toEqual(byte_0000_0101)
+})
+
+describe('test isPressed() function', () => {
+  test('test passing invalid positions to isPressed() function', () => {
+    expect(() => buttons.isPressed(4)).toThrow(
+      new Error('Button position 4 does not exist.')
+    )
+    expect(() => buttons.isPressed(-2)).toThrow(
+      new Error('Button position -2 does not exist.')
+    )
+  })
+  test('test passing valid positions to isPressed() function', () => {
+    expect(buttons.isPressed(0)).toBe(true)
+    expect(buttons.isPressed(1)).toBe(false)
+    expect(buttons.isPressed(2)).toBe(true)
+    expect(buttons.isPressed(3)).toBe(false)
+  })
+})
+
+describe('test press() function', () => {
+  test('test passing invalid positions to press() function', () => {
+    expect(() => buttons.press(4)).toThrow(
+      new Error('Button position 4 does not exist.')
+    )
+    expect(() => buttons.press(-2)).toThrow(
+      new Error('Button position -2 does not exist.')
+    )
+  })
+  test('test passing valid positions to press() function', () => {
+    buttons.press(1)
+    buttons.press(3)
+
+    expect(buttons.readByte(address)).toEqual(byte_0000_1111)
+
+    expect(buttons.isPressed(0)).toBe(true)
+    expect(buttons.isPressed(1)).toBe(true)
+    expect(buttons.isPressed(2)).toBe(true)
+    expect(buttons.isPressed(3)).toBe(true)
+  })
+  test('test passing the same value to press() function again', () => {
+    buttons.press(1)
+    expect(buttons.isPressed(1)).toBe(true)
+    buttons.press(1)
+    expect(buttons.isPressed(1)).toBe(true)
+  })
+})
+
+describe('test release() function', () => {
+  test('test passing invalid positions to release() function', () => {
+    expect(() => buttons.press(4)).toThrow(
+      new Error('Button position 4 does not exist.')
+    )
+    expect(() => buttons.press(-2)).toThrow(
+      new Error('Button position -2 does not exist.')
+    )
+  })
+  test('test passing valid positions to release() function', () => {
+    buttons.release(0)
+    buttons.release(2)
+
+    expect(buttons.readByte(address)).toEqual(byte_0000_0000)
+
+    expect(buttons.isPressed(0)).toBe(false)
+    expect(buttons.isPressed(1)).toBe(false)
+    expect(buttons.isPressed(2)).toBe(false)
+    expect(buttons.isPressed(3)).toBe(false)
+  })
+  test('test passing the same value to release() function again', () => {
+    buttons.release(0)
+    expect(buttons.isPressed(0)).toBe(false)
+    buttons.release(0)
+    expect(buttons.isPressed(0)).toBe(false)
+  })
+})
