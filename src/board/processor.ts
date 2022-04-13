54113ade39ac9d552c0e837f9f9290fb536617ce
Claudio <claudio.frei@ifolor.com>
2022-04-13 15:54:00 +0200

    Create 'step' method

--- src/board/processor.ts
+++ src/board/processor.ts
@@ -52,8 +52,8 @@ export class Processor extends EventEmitter<ProcessorEvents> {
    * @returns void
    */
   public execute(): void {
-    if (this.interval !== 0) return
-    this.interval = window.setInterval(this.cycle, cycleSpeed)
+    if (this.isRunning()) return
+    this.interval = window.setInterval(() => this.cycle(), cycleSpeed)
   }
 
   /**
@@ -63,6 +63,14 @@ export class Processor extends EventEmitter<ProcessorEvents> {
   public halt(): void {
     window.clearInterval(this.interval)
     this.interval = 0
+  }
+
+  /**
+   * Runs one processor cycle.
+   */
+  public step(): void {
+    if (this.isRunning()) return
+    this.cycle()
   }
 
   /**
@@ -91,7 +99,7 @@ export class Processor extends EventEmitter<ProcessorEvents> {
     this.emit('afterReset')
   }
 
-  private cycle = (): void => {
+  private cycle() {
     const pc: Word = this.registers.readRegister(Register.PC)
     const opcode: Halfword = this.memory.readHalfword(pc)
     if (opcode.value === END_OF_CODE.value) {
--- test/board/processor.test.ts
+++ test/board/processor.test.ts
@@ -1,33 +1,99 @@
-import { IMemory } from 'board/memory/interfaces'
-import { IInstructionSet } from 'instruction/interfaces'
+import { Halfword, Word } from 'types/binary'
 
+import { Register, Registers } from 'board/registers'
+import { MemoryBus } from 'board/memory/bus'
+import { Flash } from 'board/devices/flash'
 import { Processor } from 'board/processor'
-import { Registers } from 'board/registers'
 
-import { mock, instance, verify, anything } from 'ts-mockito'
+import { END_OF_CODE } from 'instruction/special'
 
-const instructionSetMock: IInstructionSet = mock<IInstructionSet>()
-const memoryMock: IMemory = mock<IMemory>()
-const registersMock: Registers = mock(Registers)
+import InstructionSet from 'instruction/set'
 
-test('test halt() function', () => {
-  const processor: Processor = new Processor(
-    instance(registersMock),
-    instance(memoryMock),
-    instance(instructionSetMock)
+const registers = new Registers()
+const memory = new MemoryBus([new Flash()])
+
+function MOVS(...options: string[]) {
+  return InstructionSet.getEncoder('MOVS', options).encodeInstruction(
+    options,
+    {}
   )
-  processor.halt()
+}
+
+function WRITE_CODE(opcodes: Halfword[]) {
+  memory.writeWord(
+    Word.fromUnsignedInteger(0x08000000),
+    Word.fromUnsignedInteger(0x20002000)
+  )
+  memory.writeWord(
+    Word.fromUnsignedInteger(0x08000004),
+    Word.fromUnsignedInteger(0x08000008)
+  )
+  let address = Word.fromUnsignedInteger(0x08000008)
+  for (const opcode of opcodes) {
+    memory.writeHalfword(address, opcode)
+    address = address.add(2)
+  }
+  memory.writeHalfword(address, END_OF_CODE)
+}
+
+beforeEach(function () {
+  registers.reset()
+  memory.reset()
 })
 
-test('test reset() function', () => {
-  const processor: Processor = new Processor(
-    instance(registersMock),
-    instance(memoryMock),
-    instance(instructionSetMock)
-  )
-  processor.reset()
+describe('execute', function () {
+  it('should execute code', function () {
+    WRITE_CODE([MOVS('R1', '#1')])
+    const processor: Processor = new Processor(
+      registers,
+      memory,
+      InstructionSet
+    )
+    processor.execute()
+    expect(processor.isRunning()).toBe(true)
+  })
+})
 
-  verify(memoryMock.reset()).called()
-  verify(registersMock.reset()).called()
-  verify(registersMock.writeRegister(anything(), anything())).twice()
+describe('halt', function () {
+  it('should halt execution', function () {
+    WRITE_CODE([MOVS('R1', '#1')])
+    const processor: Processor = new Processor(
+      registers,
+      memory,
+      InstructionSet
+    )
+    processor.execute()
+    expect(processor.isRunning()).toBe(true)
+    processor.halt()
+    expect(processor.isRunning()).toBe(false)
+  })
+})
+
+describe('step', function () {
+  it('should execute one instruction per step', function () {
+    WRITE_CODE([MOVS('R1', '#1'), MOVS('R2', '#2')])
+    const processor: Processor = new Processor(
+      registers,
+      memory,
+      InstructionSet
+    )
+    processor.reset()
+    expect(registers.readRegister(Register.R1).toUnsignedInteger()).toBe(0)
+    expect(registers.readRegister(Register.R2).toUnsignedInteger()).toBe(0)
+    expect(registers.readRegister(Register.PC).toUnsignedInteger()).toBe(
+      0x08000008
+    )
+    processor.step()
+    expect(registers.readRegister(Register.R1).toUnsignedInteger()).toBe(1)
+    expect(registers.readRegister(Register.R2).toUnsignedInteger()).toBe(0)
+    expect(registers.readRegister(Register.PC).toUnsignedInteger()).toBe(
+      0x0800000a
+    )
+    processor.step()
+    expect(registers.readRegister(Register.R1).toUnsignedInteger()).toBe(1)
+    expect(registers.readRegister(Register.R2).toUnsignedInteger()).toBe(2)
+    expect(registers.readRegister(Register.PC).toUnsignedInteger()).toBe(
+      0x0800000c
+    )
+  })
 })
