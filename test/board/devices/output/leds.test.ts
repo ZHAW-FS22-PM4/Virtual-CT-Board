85af8946b3bd1b2780dabe659107762f93c9ccc7
Raphael Krebs <48808406+akrosis@users.noreply.github.com>
2022-04-20 23:34:12 +0200

    Vcb 6 leds on ct board (#18)
    * basic setup
    
    * implement leds
    
    * update led naming
    
    * format code
    
    * adjust labels and correct imports
    
    * fix bord layout with led & button combination
    
    * fix update function
    
    * set led device as volatile
    
    * update name of components and device

---
+++ test/board/devices/output/leds.test.ts
@@ -0,0 +1,38 @@
+import { LEDDevice } from 'board/devices/output/leds'
+import { Byte, Word } from 'types/binary'
+
+const byte_0101_0101: Byte = Byte.fromUnsignedInteger(85)
+const byte_1010_1010: Byte = Byte.fromUnsignedInteger(170)
+const byte_0000_0000: Byte = Byte.fromUnsignedInteger(0)
+const byte_1111_1111: Byte = Byte.fromUnsignedInteger(255)
+
+let leds: LEDDevice
+
+beforeEach(() => {
+  leds = new LEDDevice()
+  leds.writeWord(
+    leds.startAddress,
+    Word.fromBytes(
+      byte_0101_0101,
+      byte_1010_1010,
+      byte_0000_0000,
+      byte_1111_1111
+    )
+  )
+})
+
+test('isOn() returns led state on correct position', () => {
+  expect(leds.isOn(0)).toBeTruthy()
+  expect(leds.isOn(1)).toBeFalsy()
+  expect(leds.isOn(7)).toBeFalsy()
+  expect(leds.isOn(8)).toBeFalsy()
+  expect(leds.isOn(17)).toBeFalsy()
+  expect(leds.isOn(18)).toBeFalsy()
+  expect(leds.isOn(30)).toBeTruthy()
+  expect(leds.isOn(31)).toBeTruthy()
+})
+
+test('isOn() throws error when position is out of bounds', () => {
+  expect(() => leds.isOn(-1)).toThrow(new Error(`Position -1 does not exist.`))
+  expect(() => leds.isOn(32)).toThrow(new Error(`Position 32 does not exist.`))
+})
