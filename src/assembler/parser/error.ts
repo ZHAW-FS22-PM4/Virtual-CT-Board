e288e97aeb15377001872ea6b9165564ed059d1b
Claudio <claudio.frei@ifolor.com>
2022-04-21 10:51:19 +0200

    Show correct line in error

--- src/assembler/parser/error.ts
+++ src/assembler/parser/error.ts
@@ -3,7 +3,7 @@ import { ITextCursor } from './text'
 export class ParseError extends Error {
   public cursor: ITextCursor
   constructor(cursor: ITextCursor, message: string) {
-    super(`${message} at Line ${cursor.line}, Position ${cursor.position}`)
+    super(`${message} at Line ${cursor.line + 1}, Position ${cursor.position}`)
     this.cursor = cursor
   }
 }
