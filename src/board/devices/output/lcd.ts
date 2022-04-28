6d581e5935b78308f3cd2472a1ddc283517056b8
Stefan Enz <enzste02@students.zhaw.ch>
2022-04-28 16:30:15 +0200

    implemented lcd device

--- src/board/devices/output/lcd.ts
+++ src/board/devices/output/lcd.ts
@@ -1,8 +1,22 @@
 import { Device } from 'board/devices/device'
-import { Halfword, Word } from 'types/binary'
+import { Byte, Halfword, Word } from 'types/binary'
+
+enum LcdPositionType {
+  NotSet = 0,
+  Ascii = 1,
+  Binary = 2
+}
+
+type ILcdPositionInfo = {
+  [index: number]: LcdPositionType
+}
+
+type ILcdBinaryFields = {
+  [index: number]: [offset: number, bytePosition: number]
+}
 
 export class LcdDisplay extends Device {
-  private static readonly MAX_LCD_TEXT_NUMBER: number = 39
+  private static readonly MAX_LCD_POSITION: number = 39
 
   private static readonly LCD_ASCII_ADRESS_LIST: Word[] = this.createAdressList(
     0x60000300,
@@ -12,6 +26,25 @@ export class LcdDisplay extends Device {
     this.createAdressList(0x60000330, 0x6000033f)
   private static readonly LCD_COLOUR_ADRESS_LIST: Word[] =
     this.createAdressList(0x60000340, 0x60000345)
+
+  private static readonly MAX_BINARY_OFFSET: number = 15
+  private static readonly EMPTY_POSITIONS_IN_BINARY_MODE: number[] = [
+    0, 5, 10, 15, 20, 25, 30, 35
+  ]
+
+  // Defines the size of a block on the LCD Screen
+  private static readonly BLOCK_SIZE: number = 5
+
+  /*  
+    This variable represents the arrangement of the binary display mode.
+    The index is the position on the lcd screen.
+    The first column (offset) defines, which offset the position belongs to and the second one (bytePosition) shows,
+    if the position shows the first or the second char of the byte
+  */
+  private static readonly LCD_BINARY_FIELD_CONFIGURATION: ILcdBinaryFields =
+    LcdDisplay.createLcdBinaryFieldConfigruation()
+
+  private lcdPositionInfo: ILcdPositionInfo = this.createLcdPositionInfo()
 
   public startAddress = LcdDisplay.LCD_ASCII_ADRESS_LIST[0]
   public endAddress = LcdDisplay.LCD_COLOUR_ADRESS_LIST[5]
@@ -28,12 +61,39 @@ export class LcdDisplay extends Device {
     if (this.invalidPosition(position)) {
       throw new Error(`Position ${position} does not exist.`)
     }
-    let t: String = String.fromCharCode(2)
-    return String.fromCharCode(
-      this.memory
-        .readByte(LcdDisplay.LCD_ASCII_ADRESS_LIST[position])
-        .toUnsignedInteger()
-    )
+    switch (this.lcdPositionInfo[position]) {
+      case LcdPositionType.NotSet: {
+        return ''
+      }
+      case LcdPositionType.Ascii: {
+        return String.fromCharCode(
+          this.memory
+            .readByte(LcdDisplay.LCD_ASCII_ADRESS_LIST[position])
+            .toUnsignedInteger()
+        )
+      }
+      case LcdPositionType.Binary: {
+        // Check if this position is an empty field (offset = -1)
+        if (LcdDisplay.LCD_BINARY_FIELD_CONFIGURATION[position][0] == -1) {
+          return ''
+        }
+        let binaryString: string = this.memory
+          .readByte(
+            LcdDisplay.LCD_BINARY_ADRESS_LIST[
+              LcdDisplay.LCD_BINARY_FIELD_CONFIGURATION[position][0]
+            ]
+          )
+          .toBinaryString()
+        // Check if position is the first half of the byte
+        let s: string
+        if (LcdDisplay.LCD_BINARY_FIELD_CONFIGURATION[position][1] == 1) {
+          s = binaryString.substring(0, 4)
+        } else {
+          s = binaryString.substring(4, 8)
+        }
+        return parseInt(s, 2).toString(16).toUpperCase()
+      }
+    }
   }
 
   /**
@@ -49,6 +109,71 @@ export class LcdDisplay extends Device {
     return colour
   }
 
+  public writeByte(address: Word, byte: Byte): void {
+    super.writeByte(address, byte)
+    this.writeAtPosition(address)
+  }
+
+  public writeBytes(address: Word, bytes: Byte[]): void {
+    super.writeBytes(address, bytes)
+    for (let i = 0; i < bytes.length; i++) {
+      this.writeAtPosition(address.add(i))
+    }
+  }
+
+  public writeHalfword(address: Word, halfword: Halfword): void {
+    super.writeHalfword(address, halfword)
+    this.writeAtPosition(address)
+    this.writeAtPosition(address.add(1))
+  }
+
+  public writeWord(address: Word, word: Word): void {
+    super.writeWord(address, word)
+    for (let i = 0; i < 4; i++) {
+      this.writeAtPosition(address.add(i))
+    }
+  }
+
+  private writeAtPosition(address: Word): void {
+    if (LcdDisplay.LCD_ASCII_ADRESS_LIST.includes(address)) {
+      this.setLcdPositionInfoBlock(
+        LcdDisplay.LCD_ASCII_ADRESS_LIST.indexOf(address),
+        LcdPositionType.Ascii
+      )
+    } else if (LcdDisplay.LCD_BINARY_ADRESS_LIST.includes(address)) {
+      this.setLcdPositionInfoBlock(
+        LcdDisplay.LCD_BINARY_ADRESS_LIST.indexOf(address),
+        LcdPositionType.Binary
+      )
+    }
+  }
+
+  private setLcdPositionInfoBlock(
+    position: number,
+    type: LcdPositionType
+  ): void {
+    if (
+      position < Math.min.apply(Math, LcdDisplay.EMPTY_POSITIONS_IN_BINARY_MODE)
+    ) {
+      throw new Error(
+        `Position ${position} is lower then the lowest empty position.`
+      )
+    }
+    let lowestPositionOfBlock = position
+    while (
+      !LcdDisplay.EMPTY_POSITIONS_IN_BINARY_MODE.includes(lowestPositionOfBlock)
+    ) {
+      lowestPositionOfBlock--
+    }
+    for (
+      let i = lowestPositionOfBlock;
+      i < lowestPositionOfBlock + LcdDisplay.BLOCK_SIZE;
+      i++
+    ) {
+      this.lcdPositionInfo[position] = type
+    }
+  }
+
   private static createAdressList(start: number, end: number): Word[] {
     let adresslist: Word[] = []
     for (let i = 0; i <= end - start; i++) {
@@ -57,7 +182,36 @@ export class LcdDisplay extends Device {
     return adresslist
   }
 
+  private static createLcdBinaryFieldConfigruation(): ILcdBinaryFields {
+    let fields: ILcdBinaryFields = []
+    let nextBytePosition: number = 1
+    let nextOffset: number = LcdDisplay.MAX_BINARY_OFFSET
+
+    for (let i = 0; i <= LcdDisplay.MAX_LCD_POSITION; i++) {
+      if (LcdDisplay.EMPTY_POSITIONS_IN_BINARY_MODE.includes(i)) {
+        fields[i] = [-1, -1]
+      } else {
+        fields[i] = [nextOffset, nextBytePosition]
+        if (nextBytePosition == 1) {
+          nextBytePosition--
+        } else {
+          nextBytePosition++
+          nextOffset--
+        }
+      }
+    }
+    return fields
+  }
+
   private invalidPosition(position: number): boolean {
-    return position < 0 || position > LcdDisplay.MAX_LCD_TEXT_NUMBER
+    return position < 0 || position > LcdDisplay.MAX_LCD_POSITION
+  }
+
+  private createLcdPositionInfo(): ILcdPositionInfo {
+    let lcdPos: ILcdPositionInfo = []
+    for (let i = 0; i <= LcdDisplay.MAX_LCD_POSITION; i++) {
+      lcdPos[i] = LcdPositionType.NotSet
+    }
+    return lcdPos
   }
 }
