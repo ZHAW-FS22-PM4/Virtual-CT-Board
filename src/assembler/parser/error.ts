8f595ecfc574fc9c8302b642ea937d0f96171dd9
m-dot-wigger <56474048+m-dot-wigger@users.noreply.github.com>
2022-05-08 22:48:23 +0200

    VCB-169 support tabs as separator and show line of encoder error (#34)
    * fix: support tabs as well
    
    * show line of encoder error

--- src/assembler/parser/error.ts
+++ src/assembler/parser/error.ts
@@ -1,3 +1,4 @@
+import { VirtualBoardError, VirtualBoardErrorType } from 'types/error'
 import { ITextCursor } from './text'
 
 export class ParseError extends Error {
@@ -5,5 +6,13 @@ export class ParseError extends Error {
   constructor(cursor: ITextCursor, message: string) {
     super(`${message} at Line ${cursor.line + 1}, Position ${cursor.position}`)
     this.cursor = cursor
+  }
+}
+
+export class CompileError extends VirtualBoardError {
+  public line: number
+  constructor(line: number, message: string, type: VirtualBoardErrorType) {
+    super(`${message} at Line ${line + 1}`, type)
+    this.line = line
   }
 }
